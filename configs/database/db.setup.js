0c4daf7cec276ef838c282f068f3c5320baf4084
Anabel Perez <anabelpdv@gmail.com>
2019-11-09 16:43:28 -0500

    db config fix

--- configs/database/db.setup.js
+++ configs/database/db.setup.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost/uxer-server', {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
+  .connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
